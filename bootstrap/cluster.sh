75e9b9c7ac87961d62a4942c35233d2722f7c720
ClementGayet <clement.gayet98@gmail.com>
2025-06-10 19:21:20 +0200

    update gateway kube

--- bootstrap/cluster.sh
+++ bootstrap/cluster.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 cat <<EOF | kind create cluster --config=-
-kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+kind: Cluster
 nodes:
   - role: control-plane
     extraPortMappings:
       - containerPort: 31437
         hostPort: 80
         protocol: TCP
-      - containerPort: 31438
-        hostPort: 443
-        protocol: TCP
-  - role: worker
-  - role: worker
 EOF
 
 kubectl apply -f - <<EOF
--- bootstrap/gateway.sh
+++ bootstrap/gateway.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 
-kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v1.6.2" | kubectl apply -f -
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.0.0" | kubectl apply -f -
 
-helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway --set service.create=false
-
-kubectl apply -f - <<EOF
-
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx-gateway
-  namespace: nginx-gateway
-  labels:
-    app.kubernetes.io/name: nginx-gateway-fabric
-    app.kubernetes.io/instance: ngf
-    app.kubernetes.io/version: "1.6.2"
-spec:
-  type: NodePort
-  selector:
-    app.kubernetes.io/name: nginx-gateway-fabric
-    app.kubernetes.io/instance: ngf
-  ports:
-    - name: http
-      port: 80
-      protocol: TCP
-      targetPort: 80
-      nodePort: 31437
-    - name: https
-      port: 443
-      protocol: TCP
-      targetPort: 443
-      nodePort: 31438
-EOF
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway --set nginx.service.type=NodePort --set-json 'nginx.service.nodePorts=[{"port":31437,"listenerPort":80}]'
 
 kubectl apply -f - <<EOF
 apiVersion: gateway.networking.k8s.io/v1
